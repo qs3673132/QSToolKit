03921bb470f8d2348fd83b75a87563acf2c6ac4a
齐嵩 <qs@qs.local>
2017-01-16 11:51:37 +0800

    Added: -URL扩展

--- QSToolKit/Catogory/NSString/NSString+QSToolKit.h
+++ QSToolKit/Catogory/NSString/NSString+QSToolKit.h
@@ -10,15 +10,30 @@
 
 @interface NSString (QSToolKit)
 
+#pragma mark - API Extension
 - (NSString *)firstChar;
 - (NSString *)lastChar;
-- (BOOL)isIntegerNumber;
+- (BOOL)isInteger;
+- (NSInteger)getInteger;
 
 
+#pragma mark - URL
+- (BOOL)isURL;
+- (NSString *)URLAppendPath:(NSString *)path;
+- (NSString *)URLAppendExt:(NSString *)ext;
 
 
+#pragma mark - Base64
+- (NSString *)base64Encoding;
+- (NSString *)base64Decoding;
+
+
+#pragma mark - MD5
 - (NSString *)MD5Hash;
 + (NSString *)MD5HashOfData:(NSData *)data;
 + (NSString *)MD5HashOfFile:(NSString *)path;
+
+
++ (NSString *)fitConvertSize:(NSInteger)size;
 
 @end
--- QSToolKit/Catogory/NSString/NSString+QSToolKit.m
+++ QSToolKit/Catogory/NSString/NSString+QSToolKit.m
@@ -9,7 +9,10 @@
 #import "NSString+QSToolKit.h"
 #import <CommonCrypto/CommonDigest.h>
 
+
 @implementation NSString (QSToolKit)
+
+#pragma mark - API Extension
 
 - (NSString *)firstChar {
     if (self.length == 0) return nil;
@@ -22,23 +25,52 @@
     return [self substringFromIndex:self.length - 1];
 }
 
-- (BOOL)isIntegerNumber {
-    NSScanner* scan = [NSScanner scannerWithString:self];
+- (BOOL)isInteger {
+    NSScanner *scan = [NSScanner scannerWithString:self];
     NSInteger val;
     return [scan scanInteger:&val] && [scan isAtEnd];
 }
 
+- (NSInteger)getInteger {
+    NSCharacterSet* nonDigits = [[NSCharacterSet decimalDigitCharacterSet] invertedSet];
+    return [[self stringByTrimmingCharactersInSet:nonDigits] integerValue];
+}
 
 
 
+#pragma mark - URL
+- (BOOL)isURL {
+    NSDataDetector *detector = [NSDataDetector dataDetectorWithTypes:NSTextCheckingTypeLink error:nil];
+    return [detector matchesInString:self options:0 range:NSMakeRange(0, self.length)].count;
+}
+
+- (NSString *)URLAppendPath:(NSString *)path {
+    return [[NSURL URLWithString:self] URLByAppendingPathComponent:path].absoluteString;
+}
+
+- (NSString *)URLAppendExt:(NSString *)ext {
+    return [[NSURL URLWithString:self] URLByAppendingPathExtension:ext].absoluteString;
+}
 
 
 
+#pragma mark - Base64
+
+- (NSString *)base64Encoding {
+    NSData *data = [self dataUsingEncoding:NSUTF8StringEncoding];
+    return [data base64EncodedStringWithOptions:0];
+}
+
+- (NSString *)base64Decoding {
+    NSData *data = [[NSData alloc] initWithBase64EncodedString:self options:0];
+    return [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+}
 
 
+
+#pragma mark - MD5
 
 - (NSString *)MD5Hash {
-    
     const char *cstr = [self UTF8String];
     unsigned char result[CC_MD5_DIGEST_LENGTH];
     CC_MD5(cstr, (CC_LONG)strlen(cstr), result);
@@ -48,7 +80,6 @@
 }
 
 + (NSString *)MD5HashOfData:(NSData *)data {
-    
     unsigned char result[CC_MD5_DIGEST_LENGTH];
     CC_MD5(data.bytes, (CC_LONG)data.length, result);
     return [NSString stringWithFormat:@"%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x",
@@ -57,7 +88,6 @@
 }
 
 + (NSString *)MD5HashOfFile:(NSString *)path {
-    
     NSFileHandle *handle = [NSFileHandle fileHandleForReadingAtPath: path];
     if (handle == nil) {
         return nil;
@@ -83,4 +113,24 @@
             result[0], result[1], result[2], result[3], result[4], result[5], result[6], result[7],
             result[8], result[9], result[10], result[11], result[12], result[13], result[14], result[15]];
 }
+
+
+
+
+
+
++ (NSString *)fitConvertSize:(NSInteger)size {
+    NSInteger k = 1024, m = 1024 * k, g = 1024 * m;
+    if (size / g) {
+        return [NSString stringWithFormat:@"%.2fGB",(size * 1.f / g)];
+    }
+    if (size / m) {
+        return [NSString stringWithFormat:@"%.2fMB",(size * 1.f / m)];
+    }
+    if (size / k) {
+        return [NSString stringWithFormat:@"%.2fKB",(size * 1.f / k)];
+    }
+    return [NSString stringWithFormat:@"%@B",@(size)];
+}
+
 @end
